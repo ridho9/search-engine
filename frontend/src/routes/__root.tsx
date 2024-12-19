a01284a12574608329859ba74f0e33324cdb831f
ridho9 <p.ridho9@gmail.com>
2024-12-19 19:18:39 +0700

    feat: hide devtool

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,38 +1,31 @@
 import * as React from "react";
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
+
 function RootComponent() {
   return (
     <>
-      {/* <div className="p-2 flex gap-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>{' '}
-        <Link
-          to="/about"
-          activeProps={{
-            className: 'font-bold',
-          }}
-        >
-          About
-        </Link>
-      </div>
-      <hr /> */}
       <div className="p-12">
         <Outlet />
       </div>
-      <TanStackRouterDevtools position="bottom-right" />
+      <React.Suspense>
+        <TanStackRouterDevtools position="bottom-right" />
+      </React.Suspense>
     </>
   );
 }
